cd22896b21df41adbcd4482c4d5c5078cd5da4dc
Annamalai Narayanan <ubuntu@ip-10-171-129-14.ap-southeast-1.compute.internal>
2018-10-28 16:20:54 +0000

    renamed tmp.y

--- src/main_batch.py
+++ src/main_batch.py
@@ -7,16 +7,18 @@ from random import shuffle, randint
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,f1_score
 from collections import Counter
+import data_loader
+from torch.utils.data import DataLoader
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+bi = True
 
-
-def test(model, test_sent_ids, y_test):
+def test_batch(model, test_sent_ids, y_test):
     model.eval()
     batch_size = len(test_sent_ids)
     sents_as_ids = torch.tensor(test_sent_ids, device=device, dtype=torch.long)
     with torch.no_grad():
-            hidden = model.initHidden(batch_size)
+            hidden = model.initHidden(batch_size, bi=bi)
             logprobs = model(sents_as_ids, hidden, is_train=False)
             topv, topi = logprobs.topk(1)
             topi = topi.squeeze().detach()  # detach from history as input
@@ -25,34 +27,40 @@ def test(model, test_sent_ids, y_test):
     f1 = f1_score(y_test,y_pred,average='macro')
     return acc, f1
 
-def get_batches(l, batch_size):
-    for i in range(0, len(l), batch_size):
-        yield l[i:i + batch_size]
 
+def test_stochastic(model, test_sent_ids, y_test):
+    model.eval()
+    sents_as_ids = [torch.tensor(s, device=device, dtype=torch.long).view(-1, 1) for s in test_sent_ids]
+    y_pred = []
+    with torch.no_grad():
+        for sentindex, sent in enumerate(sents_as_ids):
+            hidden = model.initHidden(1, bi=bi)
+            logprobs = model.forward(sent, hidden, is_train=False, is_batch=False)
+            topv, topi = logprobs.topk(1)
+            topi = topi.squeeze().detach()  # detach from history as input
+            yhat = topi.item()
+            y_pred.append(yhat)
+    acc = accuracy_score(y_test,y_pred)
+    f1 = f1_score(y_test,y_pred,average='macro')
+    return acc, f1
 
-def train(sents_as_ids, labels, embeddings, n_epochs=2, batch_size = 10, vocab_size=-1, lr=0.01, use_pretrained_wv=False, n_classes=2):
-    sent_max_len = len(sents_as_ids[0])
-    XY = list(zip(sents_as_ids, labels))
-
+def train(train_loader, embeddings, n_epochs=2, vocab_size=-1, lr=0.01, use_pretrained_wv=False, n_classes=2):
     model = rnn_model_batch.rnn_model(device, word_embedding=embeddings, n_classes=n_classes,
-                                vocab_size=vocab_size, use_pretrained_wv=use_pretrained_wv)
+                                vocab_size=vocab_size, use_pretrained_wv=use_pretrained_wv, bi=bi)
     model = model.cuda()
     print(f' created RNN (GRU) model: {model}')
     criterion = nn.NLLLoss()
     optimizer = optim.SGD(model.parameters(), lr=lr)
     model.train()
     for e in range(1,n_epochs+1):
-        shuffle(XY)
         t0 = time()
         losses = []
-        for sents_labels in get_batches(XY,batch_size):
-            sents, labels = zip(*sents_labels)
-            sents = torch.tensor(sents,device=device,dtype=torch.long)
+        for batch_data, batch_labels in train_loader:
+            sents = batch_data
+            labels = batch_labels
             optimizer.zero_grad()
-            hidden = model.initHidden(batch_size=len(labels))
-            logprobs = model(sents,hidden)
-
-            labels = torch.tensor(labels,device=device,dtype=torch.long)
+            hidden = model.initHidden(batch_size=len(labels), bi=bi)
+            logprobs = model(sents,hidden, is_batch=True)
             loss = criterion(logprobs, labels)
             losses.append(loss.item())
             loss.backward()
@@ -66,7 +74,7 @@ def train(sents_as_ids, labels, embeddings, n_epochs=2, batch_size = 10, vocab_s
 
 
 
-def main(load_embedding=False, max_runs=5):
+def main(load_embedding=False, max_runs=5,batch_size=1):
     sents, labels, n_classes = utils.load_imdb_dataset()
     # sents, labels, n_classes = utils.load_rotten_tomatoes_dataset()
     print(f'distribution of classes: {Counter(labels)}')
@@ -80,17 +88,24 @@ def main(load_embedding=False, max_runs=5):
         embeddings=None
 
     sents_as_ids = utils.convert_word_to_id(tokenized_sents, word_id_map)
-    sents_as_ids = [utils.pad_token_ids(s,word_id_map['PAD'],max_len=30) for s in sents_as_ids]
+    # sents_as_ids = [utils.pad_token_ids(s,word_id_map['PAD'],max_len=30) for s in sents_as_ids]
+
+
 
     accs = []
     f_scores = []
     for run in range(1, max_runs+1):
         train_sent_ids, test_sent_ids, y_train, y_test = train_test_split(sents_as_ids, labels, test_size = 0.3, random_state = randint(1,100))
+        imdb_dataset = data_loader.load_dataset(train_sent_ids, y_train, device)
+        train_loader = DataLoader(imdb_dataset,batch_size=batch_size, shuffle=True,num_workers=0)
         print (f'sample lengths - train: {len(y_train)} and test: {len(y_test)}')
 
-        model = train(train_sent_ids, y_train, embeddings, n_epochs=1000, vocab_size=len(word_id_map),
-                          lr=0.8, use_pretrained_wv=load_embedding, n_classes=n_classes, batch_size=1000)
-        a,f = test(model, test_sent_ids, y_test)
+        model = train(train_loader, embeddings, n_epochs=20, vocab_size=len(word_id_map),
+                          lr=0.05, use_pretrained_wv=load_embedding, n_classes=n_classes)
+        try:
+            a,f = test_batch(model, test_sent_ids, y_test)
+        except:
+            a,f = test_stochastic(model, test_sent_ids, y_test)
         accs.append(a)
         f_scores.append(f)
         print(f'run: {run}, acc: {a}, f1: {f}')
@@ -98,4 +113,4 @@ def main(load_embedding=False, max_runs=5):
 
 
 if __name__ == '__main__':
-    main(load_embedding=True,max_runs=5)
+    main(load_embedding=True,max_runs=5,batch_size=1)
